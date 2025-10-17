f4847f2b7119d1aeb772685726c8ff02ca30648a
judithMarg <judithmargaritapacollalli@gmail.com>
2025-10-17 13:14:26 -0400

    add little styles

--- src/components/layout/AppLayout.jsx
+++ src/components/layout/AppLayout.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import { Outlet } from 'react-router'
+import './styles.css'
 
 export const AppLayout = () => {
   return (
-    <div>
-        <div>My App</div>
-        <Outlet />
+    <div className='layout'>
+        <div className='header'>My App</div>
+        <div style={{display:"flex", justifyContent:"center", alignItems:"center", height:"100%"}}>
+          <Outlet />
+        </div>
+        
     </div>
   )
 }
